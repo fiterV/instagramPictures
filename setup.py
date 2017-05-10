5557e429fcce04904c7df98746043abced53adf3
fiter <vadym_prokopec@mail.ru>
2017-05-11 00:12:40 +0300

    Classifiers added

--- setup.py
+++ setup.py
@@ -3,13 +3,22 @@ from setuptools import setup
 setup(
 	name='instagramPictures',
 	packages = ['instagramPictures'],
-	version='1.03',
+	version='1.3.1',
 	description = 'Script for downloading pictures from instagram.com',
 	author = 'Vadym Prokopets',
 	author_email = 'vadym_prokopec@mail.ru',
 	url = 'https://github.com/fiterV/instagramPictures', # use the URL to the github repo
 	keywords = ['instagram', 'pictures', 'download'], # arbitrary keywords
-	classifiers = [],
+	classifiers = [
+		'Intended Audience :: Customer Service',
+		'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development',
+        'Topic :: System :: Software Distribution',
+		'Topic :: Utilities',
+	],
 
 	install_requires=[
           'selenium',
@@ -18,4 +27,5 @@ setup(
 	entry_points={
           'console_scripts': ['instagramPictures = instagramPictures.main:main']
       },
+
 )
